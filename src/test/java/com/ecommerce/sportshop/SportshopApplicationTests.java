c5a6ba2e0bf22dbefc95593bcc1b9c6c8a81a4d0
Marcin <marcinbielen995@gmail.com>
2026-06-20 13:05:12 +0200

    Init project

---
+++ src/test/java/com/ecommerce/sportshop/SportshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.sportshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
