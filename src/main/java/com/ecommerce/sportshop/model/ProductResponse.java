ba1837d01755aa43a83e02a58d02d41528fd5d1d
Marcin <marcinbielen995@gmail.com>
2026-06-21 11:35:47 +0200

    Create models

---
+++ src/main/java/com/ecommerce/sportshop/model/ProductResponse.java
@@ -0,0 +1,22 @@
+package com.ecommerce.sportshop.model;
+
+import com.ecommerce.sportshop.entity.Brand;
+import com.ecommerce.sportshop.entity.Type;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+    private Integer id;
+    private String name;
+    private String description;
+    private Double price;
+    private String pictureUrl;
+    private Brand brand;
+    private Type type;
+}
