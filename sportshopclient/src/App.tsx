f152e57cc106663a501a61fe43f245521f1a21f1
Marcin <marcinbielen995@gmail.com>
2026-06-27 15:28:18 +0200

    Implement products collection

--- sportshopclient/src/App.tsx
+++ sportshopclient/src/App.tsx
@@ -1,8 +1,29 @@
+import { useState } from "react"
+
 function App() {
+
+//Define a state variable products, using useState
+const [products, setProducts] = useState([
+  {id: 1, name: "Product 1", price: 10},
+  {id: 2, name: "Product 2", price: 20},
+  {id: 3, name: "Product 3", price: 30}
+]);
 
   return (
     <div>
       <h1>Sport shop</h1>
+      {products.map(product => (
+        <div key={product.id}>
+          <p>Name: {product.name}</p>
+          <p>Price: {product.price}</p>
+        </div>
+      ))}
+      <button onClick={() => {
+        setProducts(prevProducts => [
+          ...prevProducts, 
+          {id: prevProducts.length+1, name: `Products ${prevProducts.length+1}`, price: Math.floor(Math.random()*100)}
+        ]);
+      }}>Add Product</button>
     </div>
   )
 }
