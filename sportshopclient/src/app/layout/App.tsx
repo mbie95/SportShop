f45c6f8ff859d5249c2d54fed9019e6e3a7e4800
Marcin <marcinbielen995@gmail.com>
2026-06-30 15:38:09 +0200

    Create Catalog Component

--- sportshopclient/src/app/layout/App.tsx
+++ sportshopclient/src/app/layout/App.tsx
@@ -1,43 +1,10 @@
-import { useEffect, useState } from "react"
-import type { Product } from "../models/product";
+import Catalog from "../../features/catalog/Catalog";
 
 function App() {
-
-  //Define a state variable products, using useState
-  const [products, setProducts] = useState<Product[]>([]);
-  // useEffect(() => {
-  //   //Function to fetch the data
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch('http://localhost:8081/api/products');
-  //       if (!response.ok) {
-  //         throw new Error('Failed to fetch the data');
-  //       }
-  //       const data = await response.json();
-  //       setProducts(data.content);
-  //     } catch (error) {
-  //       console.error('Error Fetching Data: ', error);
-  //     }
-  //   }
-  //   fetchData();
-  // }, []);
-  useEffect(() => {
-    fetch('http://localhost:8081/api/products')
-    .then(response => response.json())
-    .then(data => setProducts(data.content));
-  }, []);
   return (
     <div>
       <h1>Sport shop</h1>
-      {products.map(product => (
-        <div key={product.id}>
-          <p>Name: {product.name}</p>
-          <p>Description: {product.description}</p>
-          <p>Price: ${product.price}</p>
-          <p>Brand: {product.brand}</p>
-          <p>Type: {product.type}</p>
-        </div>
-      ))}
+      <Catalog/>
     </div>
   )
 }
---
+++ sportshopclient/src/features/catalog/Catalog.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react"
+import type { Product } from "../../app/models/product";
+
+export default function Catalog() {
+    //Define a state variable products, using useState
+    const [products, setProducts] = useState<Product[]>([]);
+    // useEffect(() => {
+    //   //Function to fetch the data
+    //   const fetchData = async () => {
+    //     try {
+    //       const response = await fetch('http://localhost:8081/api/products');
+    //       if (!response.ok) {
+    //         throw new Error('Failed to fetch the data');
+    //       }
+    //       const data = await response.json();
+    //       setProducts(data.content);
+    //     } catch (error) {
+    //       console.error('Error Fetching Data: ', error);
+    //     }
+    //   }
+    //   fetchData();
+    // }, []);
+    useEffect(() => {
+        fetch('http://localhost:8081/api/products')
+        .then(response => response.json())
+        .then(data => setProducts(data.content));
+    }, []);
+
+    return (
+        <ul>
+            {products.map(product => (
+            <div key={product.id}>
+            <p>Name: {product.name}</p>
+            <p>Description: {product.description}</p>
+            <p>Price: ${product.price}</p>
+            <p>Brand: {product.brand}</p>
+            <p>Type: {product.type}</p>
+            </div>
+        ))}
+        </ul>
+    )
+}
