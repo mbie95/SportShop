3ca3e56995159598458703c51cbba8a061c9abd9
Marcin <marcinbielen995@gmail.com>
2026-07-12 10:20:10 +0200

    Create Redux Store

---
+++ sportshopclient/src/app/store/configureStore.ts
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux";
+import { basketSlice } from "../../features/basket/basketSlice.ts";
+
+export const store = configureStore({
+    reducer:{
+        basket: basketSlice.reducer
+    }
+})
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
