bce10024f576d7adadbb275fa71bfc0362d73428
Marcin <marcinbielen995@gmail.com>
2026-07-18 11:09:24 +0200

    Fetch Types, Brands, keyword searching

--- sportshopclient/src/app/api/agent.ts
+++ sportshopclient/src/app/api/agent.ts
@@ -21,7 +21,10 @@ const requests = {
 
 const Store = {
     list:() => requests.get('products'),
-    details:(id: number) => requests.get(`products/${id}`)
+    details:(id: number) => requests.get(`products/${id}`),
+    types: () => requests.get('products/types').then(types => [{ id: 0, name: 'All' }, ...types]),
+    brands: () => requests.get('products/brands').then(brands => [{ id: 0, name: 'All' }, ...brands]),
+    search: (keyword: string) => requests.get(`products?keyword=${keyword}`)
 }
 
 const Basket = {
