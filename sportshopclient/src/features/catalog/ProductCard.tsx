bfe56e417ab715f638689a02181e1473a8c304f6
Marcin <marcinbielen995@gmail.com>
2026-07-18 10:44:26 +0200

    Implement Add to Card button

--- sportshopclient/src/features/catalog/ProductCard.tsx
+++ sportshopclient/src/features/catalog/ProductCard.tsx
@@ -1,6 +1,11 @@
-import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, CircularProgress, Typography } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
 import type { Product } from "../../app/models/product";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useAppDispatch } from "../../app/store/configureStore";
+import agent from "../../app/api/agent";
+import { setBasket } from "../basket/basketSlice";
 
 interface Props {
     product : Product;
@@ -23,6 +28,20 @@ export default function ProductCard({product}: Props) {
         minimumFractionDigits: 2
       }).format(price);
     }
+
+    const [loading, setLoading] = useState(false);
+    const dispatch = useAppDispatch();
+    function addItem() {
+      setLoading(true);
+      agent.Basket.addItem(product, dispatch)
+        .then(response => {
+          console.log('New Basket:', response.basket);
+          dispatch(setBasket(response.basket));
+        })
+        .catch(error=>console.log(error))
+        .finally(()=>setLoading(false));
+    }
+
     return (
          <Card sx={{ width: "31%" }}>
              <CardHeader avatar={
@@ -46,7 +65,14 @@ export default function ProductCard({product}: Props) {
           </Typography>
         </CardContent>
         <CardActions>
-          <Button size="small">Add to cart</Button>
+          <LoadingButton
+            loading={loading}
+            onClick={addItem}
+            size="small"
+            startIcon={loading ? <CircularProgress size={20} color="inherit" /> : null}
+          >
+            Add to cart
+          </LoadingButton> 
           <Button component={Link} to={`/store/${product.id}`} size="small">View</Button>
         </CardActions>
         </Card>
