a39d4717c6636ff80e43cdda1eff65c91cec97ca
Marcin <marcinbielen995@gmail.com>
2026-07-25 10:25:01 +0200

    Create Authentication Controller

---
+++ src/main/java/com/ecommerce/sportshop/controller/AuthController.java
@@ -0,0 +1,70 @@
+package com.ecommerce.sportshop.controller;
+
+import com.ecommerce.sportshop.model.JwtRequest;
+import com.ecommerce.sportshop.model.JwtResponse;
+import com.ecommerce.sportshop.security.JwtHelper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserDetailsService userDetailsService;
+    private final AuthenticationManager manager;
+    private final JwtHelper jwtHelper;
+
+    public AuthController(UserDetailsService userDetailsService, AuthenticationManager manager, JwtHelper jwtHelper) {
+        this.userDetailsService = userDetailsService;
+        this.manager = manager;
+        this.jwtHelper = jwtHelper;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest request) {
+        this.authenticate(request.getUsername(), request.getPassword());
+        UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
+        String token = this.jwtHelper.generateToken(userDetails);
+        JwtResponse response = JwtResponse.builder()
+                .username(userDetails.getUsername())
+                .token(token)
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<UserDetails> getUserDetails(@RequestHeader("Authorization") String tokenHeader) {
+        String token = extractTokenFromHeader(tokenHeader);
+        if(token != null) {
+            String username = jwtHelper.getUserNameFromToken(token);
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            return new ResponseEntity<>(userDetails, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private void authenticate(String username, String password) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+        try {
+            manager.authenticate(authenticationToken);
+        }
+        catch(BadCredentialsException ex) {
+            throw new BadCredentialsException("Invalid UserName or Password");
+        }
+    }
+
+    private String extractTokenFromHeader(String tokenHeader) {
+        if(tokenHeader != null && tokenHeader.startsWith("Bearer ")) {
+            return tokenHeader.substring(7);
+        }
+        return null;
+    }
+
+}
