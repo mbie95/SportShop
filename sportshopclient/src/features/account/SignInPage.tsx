8d72904d77ebdf91301217b9adfbb83dcb659576
Marcin <marcinbielen995@gmail.com>
2026-07-26 08:12:30 +0200

    Create login page

--- sportshopclient/src/features/account/SignInPage.tsx
+++ sportshopclient/src/features/account/SignInPage.tsx
@@ -1,5 +1,76 @@
+import { LoadingButton } from "@mui/lab";
+import { Container, CssBaseline, Box, Avatar, Typography, TextField, FormControlLabel, Checkbox, Grid } from "@mui/material";
+import { Link } from "react-router-dom";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
 export default function SignInPage() {
     return (
-        <h1>Sign In Page</h1>
-    )
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" /*onSubmit={handleSubmit(submitForm)}*/ noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                autoFocus
+                // {...register('username', {required:'Username is required'})}
+                // error={!!errors.username}
+                // helperText={errors?.username?.message as string}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                id="password"
+                // {...register('password', {required:'Password is required'})}
+                // error={!!errors.password}
+                // helperText={errors?.password?.message as string}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <LoadingButton //loading={isSubmitting}
+                //disabled={!isValid}
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </LoadingButton>
+              <Grid container>
+                <Grid sx={{size: 'xs'}}>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid>
+                  <Link href="/register" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>    
+    );
 }
