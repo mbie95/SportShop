349bed5ee9afe3c844a8359e6345771163b520ce
Marcin <marcinbielen995@gmail.com>
2026-07-31 13:55:26 +0200

    Fetch basket and user on page refresh

--- sportshopclient/src/app/layout/App.tsx
+++ sportshopclient/src/app/layout/App.tsx
@@ -1,10 +1,35 @@
 import { Container, CssBaseline } from "@mui/material";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { getBasketFromLocalStorage } from "../util/util";
+import { useAppDispatch } from "../store/configureStore";
+import { fetchCurrentUser } from "../../features/account/accountSlice";
+import agent from "../api/agent";
+import { setBasket } from "../../features/basket/basketSlice";
+import Spinner from "./Spinner";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const basket = getBasketFromLocalStorage();
+    dispatch(fetchCurrentUser());
+    if(basket) {
+      agent.Basket.get()
+        .then(basket => dispatch(setBasket(basket)))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false))
+    } else {
+      setLoading(false);
+    }
+  })
+
+  if(loading) return <Spinner message="Getting Basket..."/>
+
   return (
     <>
       <ToastContainer position="bottom-right" hideProgressBar theme="colored"/>
