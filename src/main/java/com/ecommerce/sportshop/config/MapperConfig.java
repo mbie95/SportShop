31c330c197cadd2665f58085b30f0e6028180202
Marcin <marcinbielen995@gmail.com>
2026-08-02 11:37:24 +0200

    Create OrderMapper configuration

---
+++ src/main/java/com/ecommerce/sportshop/config/MapperConfig.java
@@ -0,0 +1,14 @@
+package com.ecommerce.sportshop.config;
+
+import com.ecommerce.sportshop.mapper.OrderMapper;
+import org.mapstruct.factory.Mappers;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+    @Bean
+    public OrderMapper orderMapper(){
+        return Mappers.getMapper(OrderMapper.class);
+    }
+}
