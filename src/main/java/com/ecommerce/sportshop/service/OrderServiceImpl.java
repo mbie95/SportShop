faba44c4e4998f9314f59f04396946524854335f
Marcin <marcinbielen995@gmail.com>
2026-08-02 12:49:44 +0200

    Create OrderServiceImpl

---
+++ src/main/java/com/ecommerce/sportshop/service/OrderServiceImpl.java
@@ -0,0 +1,98 @@
+package com.ecommerce.sportshop.service;
+
+import com.ecommerce.sportshop.entity.OrderAggregate.Order;
+import com.ecommerce.sportshop.entity.OrderAggregate.OrderItem;
+import com.ecommerce.sportshop.entity.OrderAggregate.ProductItemOrdered;
+import com.ecommerce.sportshop.mapper.OrderMapper;
+import com.ecommerce.sportshop.model.BasketItemResponse;
+import com.ecommerce.sportshop.model.BasketResponse;
+import com.ecommerce.sportshop.model.OrderDto;
+import com.ecommerce.sportshop.model.OrderResponse;
+import com.ecommerce.sportshop.repository.OrderRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@Log4j2
+public class OrderServiceImpl implements OrderService {
+    private final OrderRepository orderRepository;
+    private final BasketService basketService;
+    private final OrderMapper orderMapper;
+
+    public OrderServiceImpl(OrderRepository orderRepository, BasketService basketService, OrderMapper orderMapper) {
+        this.orderRepository = orderRepository;
+        this.basketService = basketService;
+        this.orderMapper = orderMapper;
+    }
+
+    @Override
+    public OrderResponse getOrderById(Integer orderId) {
+        Optional<Order> optionalOrder = orderRepository.findById(orderId);
+        return optionalOrder.map(orderMapper::OrderToOrderResponse).orElse(null);
+    }
+
+    @Override
+    public List<OrderResponse> getAllOrders() {
+        List<Order> orders = orderRepository.findAll();
+        return orders.stream().map(orderMapper::OrderToOrderResponse).collect(Collectors.toList());
+    }
+
+    @Override
+    public Page<OrderResponse> getAllOrders(Pageable pageable) {
+        return orderRepository.findAll(pageable).map(orderMapper::OrderToOrderResponse);
+    }
+
+
+    @Override
+    public void deleteOrder(Integer orderId) {
+        orderRepository.deleteById(orderId);
+    }
+
+    @Override
+    public Integer createOrder(OrderDto orderDto) {
+        BasketResponse basketResponse = basketService.getBasketById(orderDto.getBasketId());
+        if (basketResponse == null) {
+            log.error("Basket with ID {} not found", orderDto.getBasketId());
+            return null;
+        }
+        List<OrderItem> orderItems = basketResponse.getItems().stream()
+                .map(this::mapBasketItemToOrderItem)
+                .collect(Collectors.toList());
+
+        double subTotal = basketResponse.getItems().stream()
+                .mapToDouble(item -> item.getPrice() * item.getQuantity())
+                .sum();
+        Order order = orderMapper.orderResponseToOrder(orderDto);
+        order.setOrderItems(orderItems);
+        order.setSubTotal(subTotal);
+
+        Order savedOrder = orderRepository.save(order);
+        basketService.deleteBasketById(orderDto.getBasketId());
+        return savedOrder.getId();
+    }
+
+    private OrderItem mapBasketItemToOrderItem(BasketItemResponse basketItemResponse) {
+        if (basketItemResponse != null) {
+            OrderItem orderItem = new OrderItem();
+            orderItem.setItemOrdered(mapBasketItemToProduct(basketItemResponse));
+            orderItem.setQuantity(basketItemResponse.getQuantity());
+            return orderItem;
+        } else {
+            return null;
+        }
+    }
+
+    private ProductItemOrdered mapBasketItemToProduct(BasketItemResponse basketItemResponse) {
+        ProductItemOrdered productItemOrdered = new ProductItemOrdered();
+        productItemOrdered.setName(basketItemResponse.getName());
+        productItemOrdered.setPictureUrl(basketItemResponse.getPictureUrl());
+        productItemOrdered.setProductId(basketItemResponse.getId());
+        return productItemOrdered;
+    }
+}
