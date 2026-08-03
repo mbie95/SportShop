a82ea5e389247d8ae2c75408181f8e804cacd80d
Marcin <marcinbielen995@gmail.com>
2026-08-03 08:54:52 +0200

    Update quantity in product's cart

--- sportshopclient/src/features/catalog/ProductDetails.tsx
+++ sportshopclient/src/features/catalog/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
+import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from "@mui/material";
 // import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -6,12 +6,19 @@ import type { Product } from "../../app/models/product";
 import agent from "../../app/api/agent";
 import NotFound from "../../app/errors/NotFoundError";
 import Spinner from "../../app/layout/Spinner";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { LoadingButton } from "@mui/lab";
 
 export default function ProductDetails() {
 
+    const { basket } = useAppSelector(state=>state.basket);
+    const dispatch = useAppDispatch();
     const {id} = useParams<{id:string}>();
     const [product, setProduct] = useState<Product | null>();
     const [loading, setLoading] = useState(true);
+    const [quantity, setQuantity] = useState(0);
+    const [submitting, setSubmitting] = useState(false);
+    const item = basket?.items.find(i=> i.id === product?.id);
 
     // useEffect(()=>{
     //     axios.get(`http://localhost:8081/api/products/${id}`)
@@ -19,12 +26,19 @@ export default function ProductDetails() {
     //     .catch(error=>console.error(error))
     //     .finally(()=>setLoading(false));
     // }, [id])
-    useEffect(()=>{
-       id && agent.Store.details(parseInt(id)) 
-        .then(response=>setProduct(response))
-        .catch(error=>console.error(error))
-        .finally(()=>setLoading(false));
-    }, [id])
+    // useEffect(()=>{
+    //    id && agent.Store.details(parseInt(id)) 
+    //     .then(response=>setProduct(response))
+    //     .catch(error=>console.error(error))
+    //     .finally(()=>setLoading(false));
+    // }, [id])
+    useEffect(() => {
+        if (item) setQuantity(item.quantity);
+        id && agent.Store.details(parseInt(id))       
+            .then(response => setProduct(response))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false));
+    }, [id, item]);
 
     if(loading) return <Spinner message='Loading Product...'/>
     if(!product) return <NotFound/>
@@ -44,6 +58,37 @@ export default function ProductDetails() {
         currency: 'USD',
         minimumFractionDigits: 2
       }).format(price);
+    }
+
+    const inputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = parseInt(event.target.value);
+        if (!isNaN(value) && value > 0) {
+            setQuantity(value);
+        }
+    };
+
+    const updateQuantity = async () => {
+        try {
+            setSubmitting(true);
+            const newItem = {
+                ...product!,
+                quantity: quantity
+            };
+            if (item) {
+                const quantityDifference = quantity - item.quantity;
+                if (quantityDifference > 0) {
+                    await agent.Basket.incrementItemQuantity(item.id, quantityDifference, dispatch);
+                } else if (quantityDifference < 0) {
+                    await agent.Basket.decrementItemQuantity(item.id, Math.abs(quantityDifference), dispatch);
+                }
+            } else {
+                await agent.Basket.addItem(newItem, dispatch);
+            }
+            setSubmitting(false);
+        } catch (error) {
+            console.error("Failed to update quantity:", error);
+            setSubmitting(false);
+        }
     }
 
     return (
@@ -77,6 +122,31 @@ export default function ProductDetails() {
                         </TableBody>
                     </Table>
                 </TableContainer>
+                <Grid container spacing={2}>
+                    <Grid size={{xs: 6}}>
+                        <TextField
+                            onChange={inputChange} 
+                            variant='outlined'
+                            type='number'
+                            label='Quantity in Cart'
+                            fullWidth
+                            value={quantity}
+                        />
+                    </Grid>
+                    <Grid size={{xs: 6}}>
+                        <LoadingButton
+                            sx={{ height: '55px' }}
+                            color='primary'
+                            size='large'
+                            variant='contained'
+                            fullWidth
+                            loading={submitting}
+                            onClick={updateQuantity}
+                        >
+                            {item ? 'Update Quantity' : 'Add to Cart'}
+                        </LoadingButton>
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     )
